af965900ee7e3b06a55bb8cc91a897200b9a9f67
Austin Eng <213reeses@gmail.com>
2015-12-29 02:58:11 -0500

    page injection working

--- client/inject.js
+++ client/inject.js
@@ -1,3 +1,52 @@
+
+var dynamicStyle;
+var dynamicScript;
+
+function loadPage(id) {
+	var page = id.substring(1);
+	$(".content").html("");
+
+	if (typeof(dynamicStyle) !== "undefined") {
+		dynamicStyle.remove();
+	}
+	if (typeof(dynamicScript) !== "undefined") {
+		dynamicScript.remove();
+	}
+
+	dynamicScript = document.createElement('script');
+	dynamicScript.src = chrome.extension.getURL("script/" + page + ".js");
+	dynamicScript.onload = function() {
+		this.parentNode.removeChild(this);
+	};
+	(document.head||document.documentElement).appendChild(dynamicScript);
+	
+	dynamicStyle = document.createElement('link');
+	dynamicStyle.rel = "stylesheet";
+	dynamicStyle.type = "text/css";
+	dynamicStyle.href = chrome.extension.getURL("style/" + page + ".css");
+	(document.head||document.documentElement).appendChild(dynamicStyle);
+
+	$(".content").load(chrome.extension.getURL("views/" + page + ".html"));
+}
+
+
 $(document).ready(function() {
-	console.log("hello world");
+	var homeBtn = $('.nav > a')[0];
+	var el = $("<a href='#event-feed' class='h4m-link'> \
+		<div class='event-btn'> \
+		<img  /> \
+		</div> \
+		</a>");
+	el.insertAfter(homeBtn);
+	/*el.click(function() {
+		loadPage($(this).attr('href'));
+	});*/
+
+	if (window.location.hash) {
+		loadPage(window.location.hash);
+	}
+});
+
+$("body").on("click", ".h4m-link", function(){
+	loadPage($(this).attr('href'));
 });
